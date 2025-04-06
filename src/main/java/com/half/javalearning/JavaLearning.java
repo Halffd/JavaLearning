7f28fe7083c22615176b06d21bc9315730bd6892
Halffd <guhalfeld@hotmail.com>
2025-04-06 00:09:09 -0300

    Announcement class

--- src/main/java/com/half/javalearning/JavaLearning.java
+++ src/main/java/com/half/javalearning/JavaLearning.java
@@ -4,6 +4,10 @@
 
 package com.half.javalearning;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 /**
  *
  * @author half
@@ -12,5 +16,36 @@ public class JavaLearning {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        AnnouncementPrinter printer = new AnnouncementPrinter();
+        try {
+            printer.makeAnnouncement();
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
+
+class AnnouncementPrinter {
+
+    void printToDifferentStreams(PrintStream out, String message) {
+        out.println(message);
+    }
+
+    void makeAnnouncement() throws FileNotFoundException {
+        String announcement = "Important announcement!";
+        
+        // Print to standard output and error streams
+        printToDifferentStreams(System.out, announcement);
+        printToDifferentStreams(System.err, announcement);
+        
+        // Create a platform-independent file path
+        String filePath = "announcement.txt"; // Use relative path for multi-platform compatibility
+        PrintStream fileStream = new PrintStream(new File(filePath));
+        printToDifferentStreams(fileStream, announcement);
+        
+        // Show the absolute path of the stored file
+        File file = new File(filePath);
+        System.out.println("File stored at: " + file.getAbsolutePath());
     }
 }
